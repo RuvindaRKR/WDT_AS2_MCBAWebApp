19e42d025cb35dc67f9b01bde8d752e7e09640ea
RuvindaS3804158 <s3804158@student.rmit.edu.au>
2021-01-19 19:49:12 +1100

    Add project files.

---
+++ WDT_AS2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WDT_AS2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
