328391791946f42fb095f723b70b0d6335d8849c
RuvindaS3804158 <s3804158@student.rmit.edu.au>
2021-01-25 03:55:03 +1100

    Deposit/Withdraw done

--- WDT_AS2/Controllers/CustomerController.cs
+++ WDT_AS2/Controllers/CustomerController.cs
@@ -64,5 +64,66 @@ namespace WDT_AS2.Models
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Withdraw(int id) => View(await _context.Accounts.FindAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Withdraw(int id, decimal amount)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            var customer = await _context.Customers.FindAsync(account.CustomerID);
+
+            if (amount <= 0)
+                ModelState.AddModelError(nameof(amount), "Amount must be positive.");
+            if (amount.HasMoreThanTwoDecimalPlaces())
+                ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Amount = amount;
+                return View(account);
+            }
+
+            // Note this code could be moved out of the controller, e.g., into the Model.
+            account.Balance -= amount;
+            account.Transactions.Add(
+                new Transaction
+                {
+                    TransactionType = TransactionType.Withdraw,
+                    Amount = amount,
+                    TransactionTimeUtc = DateTime.UtcNow
+                });
+
+            if (GetTransactionCount(customer) > 4)
+            {
+                account.Transactions.Add(
+                new Transaction
+                {
+                    TransactionType = TransactionType.ServiceCharge,
+                    Amount = 0.1m,
+                    TransactionTimeUtc = DateTime.UtcNow
+                });
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public int GetTransactionCount(Customer customer)
+        {
+            int count = 0;
+
+            foreach(var account in customer.Accounts)
+            {
+                foreach (var transaction in account.Transactions)
+                {
+                    if ((transaction.TransactionType == TransactionType.Transfer || transaction.TransactionType == TransactionType.Withdraw) && transaction.DestinationAccountNumber != transaction.AccountNumber)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
     }
 }
