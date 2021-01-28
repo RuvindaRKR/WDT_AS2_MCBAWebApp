7e2a73c5bd91c1dcfdcd024cf1701e5c23f30eaf
Luke <googs7894@gmail.com>
2021-01-28 19:27:47 +1100

    scheduled payments showing now

--- WDT_AS2/Utilities/MiscellaneousExtensionUtilities.cs
+++ WDT_AS2/Utilities/MiscellaneousExtensionUtilities.cs
@@ -5,4 +5,11 @@
         public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
         public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
     }
+
+    public enum Period
+    {
+        Monthly = 1,
+        Quaterly = 2,
+        OnceOff = 3,
+    }
 }
---
+++ WDT_AS2/ViewModels/ScheduledPaymentsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using WDT_AS2.Utilities;
+
+namespace WDT_AS2.ViewModels
+{
+    public class ScheduledPaymentsViewModel
+    {
+        public string PayeeName { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Amount { get; init; }
+
+        public DateTime ScheduleDate { get; init; }
+
+        public Period Period { get; init; }
+    }
+}
