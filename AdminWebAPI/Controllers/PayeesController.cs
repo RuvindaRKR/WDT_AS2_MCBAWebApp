4c79cf6e4c400743b95c0bdb0f9052ab158525df
Luke <googs7894@gmail.com>
2021-01-31 22:10:02 +1100

    api updates for payee

---
+++ AdminWebAPI/Controllers/PayeesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdminWebAPI.Models;
+using AdminWebAPI.Models.DataManager;
+
+namespace AdminWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PayeesController : ControllerBase
+    {
+        private readonly PayeeManager _repo;
+
+        public PayeesController(PayeeManager repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: /Payees
+        [HttpGet]
+        public IEnumerable<Payee> Get()
+        {
+            return _repo.GetAll();
+        }
+
+        //GET: /Payees/1
+        [HttpGet("{id}")]
+        public Payee Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        // POST /Payees
+        [HttpPost]
+        public void Post([FromBody] Payee payee)
+        {
+            _repo.Add(payee);
+        }
+
+        // PUT /Payees
+        [HttpPut]
+        public void Put([FromBody] Payee payee)
+        {
+            _repo.Update(payee.PayeeID, payee);
+        }
+
+        // DELETE /Payees/1
+        [HttpDelete("{id}")]
+        public long Delete(int id)
+        {
+            return _repo.Delete(id);
+        }
+    }
+}
---
+++ AdminWebAPI/Models/DataManager/PayeeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdminWebAPI.Data;
+using AdminWebAPI.Models.Repository;
+
+namespace AdminWebAPI.Models.DataManager
+{
+    public class PayeeManager : IDataRepository<Payee, int>
+    {
+        private readonly McbaContext _context;
+
+        public PayeeManager(McbaContext context)
+        {
+            _context = context;
+        }
+
+        public Payee Get(int id)
+        {
+            return _context.Payees.Find(id);
+        }
+
+        public IEnumerable<Payee> GetAll()
+        {
+            return _context.Payees.ToList();
+        }
+
+        public int Add(Payee payee)
+        {
+            _context.Payees.Add(payee);
+            _context.SaveChanges();
+
+            return payee.PayeeID;
+        }
+
+        public int Delete(int id)
+        {
+            _context.Payees.Remove(_context.Payees.Find(id));
+            _context.SaveChanges();
+
+            return id;
+        }
+
+        public int Update(int id, Payee payee)
+        {
+            _context.Update(payee);
+            _context.SaveChanges();
+            
+            return id;
+        }
+    }
+}
